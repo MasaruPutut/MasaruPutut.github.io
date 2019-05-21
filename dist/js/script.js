d425cdfe3517f3f38d1fff0fc387de7d48b31532
Putut Dewantoro <PututDewantoro@outlook.com>
2019-05-21 23:32:52 +0700

    new update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,32 @@
 document.getElementById('w-item1').addEventListener('click', 
 function(){
+    document.querySelector('.modal').style.display = 'flex';
     document.querySelector('#modal-item-1').style.display = 'flex';
 })
 
 document.getElementById('w-item2').addEventListener('click', 
 function(){
+    document.querySelector('.modal').style.display = 'flex';
     document.querySelector('#modal-item-2').style.display = 'flex';
 })
 
 document.getElementById('w-item3').addEventListener('click', 
 function(){
+    document.querySelector('.modal').style.display = 'flex';
     document.querySelector('#modal-item-3').style.display = 'flex';
 })
 
 document.getElementById('w-item4').addEventListener('click', 
 function(){
+    document.querySelector('.modal').style.display = 'flex';
     document.querySelector('#modal-item-4').style.display = 'flex';
 })
 
 document.querySelector('.exit').addEventListener('click',
 function(){
-    document.querySelector('.bg-modal').style.display = 'none';
+    document.querySelector('.modal').style.display = 'none';
+    document.querySelector('#modal-item-1').style.display = 'none';
+    document.querySelector('#modal-item-2').style.display = 'none';
+    document.querySelector('#modal-item-3').style.display = 'none';
+    document.querySelector('#modal-item-4').style.display = 'none';
 })
