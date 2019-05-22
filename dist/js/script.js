5e344ba5aacc1cbfa5c97bae39b28cdd55aebb5a
Putut Dewantoro <PututDewantoro@outlook.com>
2019-05-22 15:42:41 +0700

    update 22 May 2019

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,32 +1,54 @@
-document.getElementById('w-item1').addEventListener('click', 
-function(){
-    document.querySelector('.modal').style.display = 'flex';
-    document.querySelector('#modal-item-1').style.display = 'flex';
-})
+// variable
 
-document.getElementById('w-item2').addEventListener('click', 
-function(){
-    document.querySelector('.modal').style.display = 'flex';
-    document.querySelector('#modal-item-2').style.display = 'flex';
-})
+const workItem1 = document.getElementById('w-item1');
+const workItem2 = document.getElementById('w-item2');
+const workItem3 = document.getElementById('w-item3');
+const workItem4 = document.getElementById('w-item4');
 
-document.getElementById('w-item3').addEventListener('click', 
-function(){
-    document.querySelector('.modal').style.display = 'flex';
-    document.querySelector('#modal-item-3').style.display = 'flex';
-})
+const modalItem1 = document.getElementById('modal-item-1');
+const modalItem2 = document.getElementById('modal-item-2');
+const modalItem3 = document.getElementById('modal-item-3');
+const modalItem4 = document.getElementById('modal-item-4');
 
-document.getElementById('w-item4').addEventListener('click', 
+const modal = document.querySelector('.modal');
+const bgModal = document.querySelector('.bg-modal');
+
+
+function popUp(modal, item){
+    modal.style.display = 'flex';
+    modalItem1.style.display = 'flex';
+}
+// modal pop up
+workItem1.addEventListener('click', 
 function(){
-    document.querySelector('.modal').style.display = 'flex';
-    document.querySelector('#modal-item-4').style.display = 'flex';
-})
+    modal.style.display = 'flex';
+    modalItem1.style.display = 'flex';
+});
+
+workItem2.addEventListener('click', 
+function(){
+    modal.style.display = 'flex';
+    modalItem2.style.display = 'flex';
+});
+
+workItem3.addEventListener('click', 
+function(){
+    modal.style.display = 'flex';
+    modalItem3.style.display = 'flex';
+});
+
+workItem4.addEventListener('click', 
+function(){
+    modal.style.display = 'flex';
+    modalItem4.style.display = 'flex';
+});
 
 document.querySelector('.exit').addEventListener('click',
 function(){
-    document.querySelector('.modal').style.display = 'none';
-    document.querySelector('#modal-item-1').style.display = 'none';
-    document.querySelector('#modal-item-2').style.display = 'none';
-    document.querySelector('#modal-item-3').style.display = 'none';
-    document.querySelector('#modal-item-4').style.display = 'none';
-})
+    modal.style.display = 'none';
+    modalItem1.style.display = 'none';
+    modalItem2.style.display = 'none';
+    modalItem3.style.display = 'none';
+    modalItem4.style.display = 'none';
+});
+
